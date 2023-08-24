7abce1dc9bc63eb93c6dacf384bd11280fbb29ba
doan cao son <son47168@donga.edu.vn>
2023-08-24 17:03:46 +0700

    fixslider

--- src/component/Slidertexxt.jsx
+++ src/component/Slidertexxt.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { motion } from "framer-motion";
-
 const Slidertexxt = ({ images, currentIndex, ref1 }) => {
   return (
     <div className="text-left w-[433px] absolute top-20 left-20">
@@ -10,6 +8,7 @@ const Slidertexxt = ({ images, currentIndex, ref1 }) => {
         }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.6 }}
+        key={images[currentIndex].title}
         className="text-[28px]"
       >
         {images[currentIndex].title}
@@ -20,17 +19,18 @@ const Slidertexxt = ({ images, currentIndex, ref1 }) => {
         }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
+        key={images[currentIndex].name}
         className="text-[56px] text-[#ffb500] w-[217px]"
       >
         {images[currentIndex].name}
       </motion.div>
       <motion.div
-        key={currentIndex}
         initial={{
           y: ref1.current === "prev" ? -90 : -90,
         }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1 }}
+        key={images[currentIndex].description}
         className="text-[18px] text-[#ffb500]"
       >
         {images[currentIndex].description}
