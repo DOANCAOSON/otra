4c1c6adb6e29eb7ab106bdd26f263cf6af76a5ca
doan cao son <son47168@donga.edu.vn>
2023-09-08 08:52:41 +0700

    addfix

--- src/page/Hiring.jsx
+++ src/page/Hiring.jsx
@@ -1,3 +1,41 @@
+const items = [
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2019/10/main-sec04_img.jpg",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis.",
+    des: "Description 1",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 2",
+    des: "Description 2",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 3",
+    des: "Description 3",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 4",
+    des: "Description 4",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 5",
+    des: "Description 5",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 6",
+    des: "Description 6",
+  },
+];
 const Hiring = () => {
   return (
     <div className="flex justify-center">
@@ -24,30 +62,31 @@ const Hiring = () => {
 
         <div>
           <div className="grid grid-cols-3 gap-x-[30px] gap-y-[26px] mb-[228px]">
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>{" "}
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
+            {items.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="h-[466px] w-[380px] bg-[#eee] "
+                >
+                  <div className="w-[100%] h-[348px] bg-[#eee] ">
+                    <div className="h-[100%]">
+                      <img
+                        style={{
+                          objectFit: "cover",
+                          width: "100%",
+                          height: "100%",
+                        }}
+                        src={item.img}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                  <div className="px-[20px]  h-[118px] w-[100%] bg-[#D9D9D9] ">
+                    {item.title}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
--- src/page/News.jsx
+++ src/page/News.jsx
@@ -1,3 +1,42 @@
+const items = [
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis.",
+    des: "Description 1",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 2",
+    des: "Description 2",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 3",
+    des: "Description 3",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 4",
+    des: "Description 4",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 5",
+    des: "Description 5",
+  },
+  {
+    img: "http://auntieannes.co.kr/wp-content/uploads/2023/08/%EB%8C%80%EC%A7%80-341-705x453.png",
+    title:
+      "Lorem ipsum dolor sit amet consectetur. Platea cras turpis libero turpis. 6",
+    des: "Description 6",
+  },
+];
+
 const News = () => {
   return (
     <div className="flex justify-center">
@@ -16,37 +55,48 @@ const News = () => {
           </div>
         </div>
         <div>
-          <div className=" w-[100%] h-[486px] bg-[#EEE] relative mb-[157px]">
-            <div className="w-[989px] h-[183px] bg-[#D9D9D9] absolute bottom-[-92px] left-[10%] "></div>
+          <div
+            style={{
+              backgroundImage:
+                "url(https://phela.vn/wp-content/uploads/2022/01/banner-1.jpg)",
+            }}
+            className="w-[100%] h-[486px] bg-[#EEE] relative mb-[157px] bg-cover bg-center"
+          >
+            <div className="w-[989px] h-[183px] bg-[#D9D9D9] flex justify-center items-center absolute bottom-[-92px] left-[10%] ">
+              <h1 className="text-[32px] text-center ">
+                Tuyển dụng có thể liên quan đến quá trình
+                tìm kiếm và lựa chọn người để làm việc trong
+                một tổ chức hoặc công ty.
+              </h1>
+            </div>
           </div>
         </div>
 
         <div>
           <div className="grid grid-cols-3 gap-x-[30px] gap-y-[26px] mb-[228px]">
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>{" "}
-            <div className="h-[466px] w-[380px] bg-[#eee] ">
-              <div className="h-[348px] w-[100%] bg-[#eee] "></div>
-              <div className="h-[118px] w-[100%] bg-[#D9D9D9] "></div>
-            </div>
+            {items.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="h-[466px] w-[380px] bg-[#eee] "
+                >
+                  <div className="w-[100%] bg-[#eee] ">
+                    <img
+                      style={{
+                        objectFit: "contain",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                      src={item.img}
+                      alt=""
+                    />
+                  </div>
+                  <div className="px-[20px]  h-[118px] w-[100%] bg-[#D9D9D9] ">
+                    {item.title}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
